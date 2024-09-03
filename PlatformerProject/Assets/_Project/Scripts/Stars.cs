9a6362a03f2e7b7c07d8552d703aa5b6c7493301
Destroliedel22 <Jordiwalet@gmail.com>
2024-09-03 15:57:03 +0200

    fences around parkour 1

--- PlatformerProject/Assets/_Project/Scripts/Stars.cs
+++ PlatformerProject/Assets/_Project/Scripts/Stars.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //text met star collected
+            //start collected aan het begin van het level
+        }
+    }
 }
