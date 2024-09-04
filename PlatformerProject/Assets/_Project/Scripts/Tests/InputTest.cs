05ba512be9005a1cc1580c50cb929d69d4f44b20
Destroliedel22 <Jordiwalet@gmail.com>
2024-09-04 14:32:28 +0200

    test with embedded input action

---
+++ PlatformerProject/Assets/_Project/Scripts/Tests/InputTest.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    public InputAction jumpAction;
+
+    private void Start()
+    {
+        jumpAction.performed += Jump;
+        //jumpAction.canceled += StopJump;
+        jumpAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.performed -= Jump;
+        //jumpAction.canceled -= StopJump;
+        jumpAction.Disable();
+    }
+
+    private void Jump(InputAction.CallbackContext value)
+    {
+        Debug.Log("Jump");
+    }
+
+
+
+
+
+
+
+    private void Update()
+    {
+        Keyboard mKeyboard = Keyboard.current;
+
+        if(mKeyboard != null )
+        {
+            if(mKeyboard.spaceKey.wasPressedThisFrame)
+            {
+                //Debug.Log("SpaceKey was pressed");
+            }
+
+            if(mKeyboard.spaceKey.isPressed)
+            {
+                //Debug.Log("SpaceKey is being pressed");
+            }
+
+            if (mKeyboard.spaceKey.wasReleasedThisFrame)
+            {
+                //Debug.Log("SpaceKey was released");
+            }
+        }
+    }
+}
