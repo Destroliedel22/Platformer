3d9be6315b9e7ec5c36ce3445d422af960ae0534
Destroliedel22 <Jordiwalet@gmail.com>
2024-09-11 15:44:19 +0200

    game loop

---
+++ PlatformerProject/Assets/_Project/Scripts/Canvas/ButtonManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        StartCoroutine(WaitToLoadScene());
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator WaitToLoadScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
+    }
+}
