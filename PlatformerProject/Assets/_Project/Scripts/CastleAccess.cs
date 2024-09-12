6b18dcb0d71e293caa3bff99d0e11ad7ae7c1939
Destroliedel22 <Jordiwalet@gmail.com>
2024-09-12 11:46:20 +0200

    castle canvas updates, jump animation faster

---
+++ PlatformerProject/Assets/_Project/Scripts/CastleAccess.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CastleAccess : MonoBehaviour
+{
+    public TextMeshProUGUI CrownsNeeded;
+
+    private void FixedUpdate()
+    {
+        CrownsNeeded.text = ScoreManager.Instance.crownAmount + "/5";
+    }
+}
