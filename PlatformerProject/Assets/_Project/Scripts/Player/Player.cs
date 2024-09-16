fbdc38d4314dd62d16a927f6ee0b8603de7596a5
Destroliedel22 <Jordiwalet@gmail.com>
2024-09-16 10:01:03 +0200

    player has health

--- PlatformerProject/Assets/_Project/Scripts/Player/Player.cs
+++ PlatformerProject/Assets/_Project/Scripts/Player/Player.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float playerHealth;
+
+    public RectTransform Health;
+    public TextMeshProUGUI HPText;
+
+    private void FixedUpdate()
+    {
+        Health.sizeDelta = new Vector2(playerHealth, Health.rect.height);
+        HPText.text = "HP:" + playerHealth;
+
+        if(playerHealth > 200)
+        {
+            playerHealth = 200;
+        }
+
+        if(playerHealth <= 0)
+        {
+            Debug.Log("You Died");
+        }
+    }
 }
